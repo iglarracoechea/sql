7e7b8dafde114167b2ebcad33a65321635ef76a1
iglarracoechea <46580285+iglarracoechea@users.noreply.github.com>
2020-09-02 22:20:51 -0300

    Update intro.sql

--- intro.sql
+++ intro.sql
@@ -24,7 +24,7 @@ logicos:and or not
 
 orden de escritura 
 
-comando + From + Where + Group by + Having + Order by
+select + From + Where + Group by + Having + Order by
 
 * para ver todos los campos
 
@@ -36,7 +36,6 @@ where sección = "CERÁMICA"
 
 -- en este caso no muestra nada porque el campo no puede tener dos secciones a la vez
 SELECT nombreartículo , sección , precio From  productos where sección = "CERÁMICA" AND sección = "DEPORTES"
-
 
 -- este es el correcto
 
@@ -67,3 +66,68 @@ SELECT SECCIÓN , PRECIO FROM `productos` WHERE SECCIÓN = 'DEPORTES' AND PRECIO
 
 SELECT * FROM `productos` WHERE FECHA BETWEEN '2000-03-01' AND '2000-05-31'
 SELECT * FROM `productos` WHERE FECHA >='2000-03-01' AND FECHA <= '2000-05-31' -- OTRA FORMA
+
+
+-- order by. Va en ultimo lugar. asc viene por defecto. ORDENA EN LA CONSULTA
+
+SELECT * FROM productos WHERE SECCIÓN = 'Deportes' or SECCIÓN = 'Cerámica' ORDER BY SECCIÓN ASC
+SELECT * FROM productos WHERE SECCIÓN = 'Deportes' or SECCIÓN = 'Cerámica' ORDER BY PRECIO DESC
+
+
+-- primero lo ordena por sección, luego ordena cada seccion por precio.
+SELECT * FROM productos WHERE SECCIÓN = 'Deportes' or SECCIÓN = 'Cerámica' ORDER BY SECCIÓN, PRECIO
+
+-- se puede establecer más criterios de ordenacion
+
+ SELECT * FROM productos WHERE SECCIÓN = 'Deportes' or SECCIÓN = 'Cerámica' ORDER BY SECCIÓN, PAÍSDEORIGEN, PRECIO
+
+/*consultas de agrupación o totales. 
+
+se puede utilizar más de un criterio de agrupación(group by) o totales
+
+el having sustituye al where en las consultas de agrupación
+usarlo con dos campos select
+
+Funciones de agregado
+
+AVG: CALCULA PROMEDIO
+COUNT: CONTADOR
+SUM: SUMA
+MAX: MAXIMO
+MIN: MINIMO
+
+*/
+
+
+SELECT SECCIÓN, SUM(PRECIO) FROM productos GROUP BY SECCIÓN
+
+-- NO FUNCIONA CORRECTAMENTE, SE SOLUCIONA CON UN ALIAS
+SELECT SECCIÓN, SUM(PRECIO) FROM productos GROUP BY SECCIÓN ORDER BY PRECIO
+
+SELECT SECCIÓN, SUM(PRECIO)AS SUMA_ARTICULOS FROM productos GROUP BY SECCIÓN ORDER BY SUMA_ARTICULOS DESC --CORRECIÓN
+
+
+-- Con un tercer campo MUESTRA PRIMER ARTICULO DE CADA SECCIÓN DE LA TABLA
+SELECT SECCIÓN, NOMBREARTÍCULO, SUM(PRECIO)AS SUMA_ARTICULOS FROM productos GROUP BY SECCIÓN ORDER BY SUMA_ARTICULOS
+
+--promedio y suma
+
+SELECT SECCIÓN, AVG(PRECIO)AS AVG_ARTICULOS, SUM(PRECIO) as suma_articulos FROM productos 
+GROUP BY SECCIÓN ORDER BY AVG_ARTICULOS, suma_articulos
+
+--group by + having + avg
+
+SELECT SECCIÓN, AVG(PRECIO)AS AVG_articulos FROM productos 
+GROUP by SECCIÓN HAVING SECCIÓN = 'Deportes' or SECCIÓN = 'Cerámica' ORDER BY AVG_ARTICULOS
+
+--group by + count(NO CUENTA CAMPOS NULLS)
+
+SELECT SECCIÓN, count(CÓDIGOARTÍCULO)AS contador_articulos FROM productos GROUP BY SECCIÓN HAVING not SECCIÓN= 'Deportes'
+SELECT POBLACIÓN, COUNT(CÓDIGOCLIENTE) AS NUM_CLIENTES FROM clientes GROUP BY POBLACIÓN
+
+--producto más caro de cada sección + max
+
+SELECT SECCIÓN, MAX(PRECIO) AS maximo_precio FROM productos GROUP BY SECCIÓN having sección = 'confección'
+
+--incorrecto, porque tiene un tercer campo. muestra el primer articulo de la sección(mal)
+SELECT SECCIÓN, NOMBREARTÍCULO, MAX(PRECIO) AS maximo_precio FROM productos WHERE SECCIÓN = 'Confección' GROUP BY SECCIÓN
