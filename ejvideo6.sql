a846fd08bf30099507905f0fe9ecdd2691fad754
iglarracoechea <46580285+iglarracoechea@users.noreply.github.com>
2020-09-04 01:44:15 -0300

    Add files via upload

---
+++ ejvideo6.sql
@@ -0,0 +1,34 @@
+/*
+1-Realizar una consulta que visualice los campos NOMBRE ARTÍCULO,
+SECCIÓN, PRECIO de la tabla PRODUCTOS y un campo nuevo que
+nombramos con el texto “DESCUENTO_7”. Debe mostrar el resultado de aplicar
+sobre el campo PRECIO un descuento de un 7 %. El formato del nuevo campo
+para debe aparecer con 2 lugares decimales.
+*/
+
+SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, round(PRECIO-(PRECIO*0.07),2) as Descuento_7  FROM  productos
+
+/*
+Realizar una consulta visualizando los campos FECHA, SECCIÓN, NOMBRE
+ARTÍCULO y PRECIO de la tabla PRODUCTOS y un campo nuevo que
+nombramos con el texto “DTO2 €_EN_CERÁMICA”. Debe mostrar el resultado
+de aplicar sobre el campo PRECIO la resta de 2 € sólo a los artículos de la sección
+CERÁMICA. El formato del nuevo campo debe aparecer con 2 lugares decimales.
+Ordenar el resultado de la consulta por el campo FECHA descendente. 
+*/
+
+SELECT FECHA, SECCIÓN, NOMBREARTÍCULO, PRECIO, round(PRECIO-2,2) as DTO2€_EN_CERÁMICA FROM productos
+WHERE SECCIÓN = 'Cerámica'
+ORDER by FECHA DESC
+
+/*
+Realizar una consulta visualizando los campos NOMBRE ARTÍCULO,
+SECCIÓN, PRECIO de la tabla PRODUCTOS y un campo nuevo que
+nombramos con el texto “PRECIO_AUMENTADO_EN_2”. Debe mostrar el
+PRECIO con un incremento de un 2% del PRECIO. Sólo debemos tener en cuenta
+los registros de la sección FERRETERÍA. El nuevo campo debe aparecer en Euros
+y con 2 lugares decimales. 
+*/
+
+SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, round(PRECIO*1.02,2) as PRECIO_AUMENTADO_EN_2 FROM productos
+WHERE SECCIÓN ='Ferretería';
